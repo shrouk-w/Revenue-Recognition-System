b91902097b15370042e4bad1a5d2210bf2073743
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-06-16 18:27:27 +0200

    added Customer Db Table

--- Revenue_Recognition_System/DAL/RRSDbContext.cs
+++ Revenue_Recognition_System/DAL/RRSDbContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Revenue_Recognition_System.Model;
 
 namespace Revenue_Recognition_System.DAL;
 
 public class RRSDbContext : DbContext
 {
+    DbSet<Customer> Customers { get; set; }
+    
     protected RRSDbContext()
     {
     }
